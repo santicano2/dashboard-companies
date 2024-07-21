4453c85c4cfd56ac8c7a2e0d9e91310029e4dd07
Santi <canosantiago404@gmail.com>
2024-07-21 20:27:57 -0300

    autentificacion con clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,13 @@
+export default function LayoutAuth({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col justify-center h-full items-center">
+      <h1 className="text-3xl my-2">Bienvenido 👋</h1>
+      <h2 className="text-2xl mb-3">Dashboard Companies</h2>
+      {children}
+    </div>
+  );
+}
