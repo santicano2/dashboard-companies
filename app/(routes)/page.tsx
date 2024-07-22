bf187d569e43e4f577a3c1c67dd10f2123ff0896
Santi <canosantiago404@gmail.com>
2024-07-22 16:53:28 -0300

    añadido navbar

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -30,7 +30,6 @@ export const dataCardsSummary = [
 export default function Home() {
   return (
     <div>
-      <UserButton />
       <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
         {dataCardsSummary.map(
