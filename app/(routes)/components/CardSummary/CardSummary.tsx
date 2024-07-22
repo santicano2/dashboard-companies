68c28090ec844701bf045e7d59a7823e60fc9cff
Santi <canosantiago404@gmail.com>
2024-07-22 16:33:14 -0300

    agregado iconos a cardSummary

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,6 +1,8 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 import { CustomTooltip } from "@/components/CustomTooltip";
+import { cn } from "@/lib/utils";
+import { MoveDownRight, MoveUpRight, TrendingUp } from "lucide-react";
 
 export function CardSummary(props: CardSummaryProps) {
   const { average, icon, total, title, tooltipText } = props;
@@ -13,6 +15,25 @@ export function CardSummary(props: CardSummaryProps) {
           {title}
         </div>
         <CustomTooltip content={tooltipText} />
+      </div>
+      <div className="flex gap-4 mt-2 md:mt-4">
+        <p className="text-2xl">{total}</p>
+        <div
+          className={cn(
+            "flex items-center gap-1 px-2 text-xs text-white rounded-lg h-[20px] bg-black dark:bg-secondary"
+          )}
+        >
+          {average}%
+          {average < 20 && (
+            <MoveDownRight strokeWidth={2} className="h-4 w-4" />
+          )}
+          {average > 20 && average < 70 && (
+            <MoveUpRight strokeWidth={2} className="h-4 w-4" />
+          )}
+          {average > 70 && average < 100 && (
+            <TrendingUp strokeWidth={2} className="h-4 w-4" />
+          )}
+        </div>
       </div>
     </div>
   );
