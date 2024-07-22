38b049f97cc4431d62ef0a45c927797bc89c31fe
Santi <canosantiago404@gmail.com>
2024-07-22 15:31:47 -0300

    modificado cardSummary

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,3 +1,19 @@
-export default function CardSummary() {
-  return <div>CardSummary</div>;
+import { CustomIcon } from "@/components/CustomIcon";
+import { CardSummaryProps } from "./CardSummary.types";
+import { CustomTooltip } from "@/components/CustomTooltip";
+
+export function CardSummary(props: CardSummaryProps) {
+  const { average, icon, total, title, tooltipText } = props;
+
+  return (
+    <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
+      <div className="flex justify-between">
+        <div className="flex gap-2 items-center">
+          <CustomIcon icon={icon} />
+          {title}
+        </div>
+        <CustomTooltip content={tooltipText} />
+      </div>
+    </div>
+  );
 }
