88b9adb70e975b8fea47a1a8fef66e70835fe426
Santi <canosantiago404@gmail.com>
2024-07-22 15:36:54 -0300

    añadida data a cardSummary

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,31 @@
 import { UserButton } from "@clerk/nextjs";
-import CardSummary from "./components/CardSummary/CardSummary";
+import { BookOpenCheck, UsersRound, Waypoints } from "lucide-react";
+
+import { CardSummary } from "./components/CardSummary/CardSummary";
+
+export const dataCardsSummary = [
+  {
+    icon: UsersRound,
+    total: "12.450",
+    average: 15,
+    title: "Companies created",
+    tooltipText: "See all of the companies created",
+  },
+  {
+    icon: Waypoints,
+    total: "86.5%",
+    average: 80,
+    title: "Total Revenue",
+    tooltipText: "See all of the summary",
+  },
+  {
+    icon: BookOpenCheck,
+    total: "363.95$",
+    average: 30,
+    title: "Bounce Rate",
+    tooltipText: "See all of the bounce rate",
+  },
+];
 
 export default function Home() {
   return (
@@ -7,9 +33,18 @@ export default function Home() {
       <UserButton />
       <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <CardSummary />
-        <CardSummary />
-        <CardSummary />
+        {dataCardsSummary.map(
+          ({ icon, total, average, title, tooltipText }) => (
+            <CardSummary
+              key={title}
+              icon={icon}
+              total={total}
+              average={average}
+              title={title}
+              tooltipText={tooltipText}
+            />
+          )
+        )}
       </div>
     </div>
   );
