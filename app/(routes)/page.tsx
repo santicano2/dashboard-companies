0ca66af78d404290804e26d2f73ccff22d5ddaf5
Santi <canosantiago404@gmail.com>
2024-07-23 15:11:49 -0300

    añadido componente de totalSuscribers

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,7 @@ import { BookOpenCheck, UsersRound, Waypoints } from "lucide-react";
 import { CardSummary } from "./components/CardSummary/CardSummary";
 import { LastCustomers } from "./components/LastCustomers";
 import { SalesDistributors } from "./components/SalesDistributors";
+import { TotalSuscribers } from "./components/TotalSuscribers";
 
 export const dataCardsSummary = [
   {
@@ -49,6 +50,11 @@ export default function Home() {
       <div className="grid grid-cols-1 xl:grid-cols-2 md:gap-x-10 mt-12">
         <LastCustomers />
         <SalesDistributors />
+      </div>
+
+      <div className="flex-col xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-center">
+        <TotalSuscribers />
+        <p>list integrations</p>
       </div>
     </div>
   );
