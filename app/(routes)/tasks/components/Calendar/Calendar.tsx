30f28c8edccb59220a2095a9fe1c696ba63b2372
Santi <canosantiago404@gmail.com>
2024-07-27 10:49:34 -0300

    componente calendar esqueleto

---
+++ app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -0,0 +1,6 @@
+import { CalendarProps } from "./Calendar.types";
+
+export function Calendar(props: CalendarProps) {
+  const { companies, events } = props;
+  return <div>Calendar</div>;
+}
---
+++ app/(routes)/tasks/page.tsx
@@ -0,0 +1,37 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { Calendar } from "./components/Calendar";
+
+export default async function TasksPage() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const companies = await db.company.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const events = await db.event.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  console.log(events);
+
+  return (
+    <div>
+      <Calendar companies={companies} events={events} />
+    </div>
+  );
+}
